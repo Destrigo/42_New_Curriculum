28f1ee5483fb3bbed65b5bf6550deba34d15ea93
Marco Tarantino <mtaranti@f1r1s18.codam.nl>
2026-01-27 15:28:20 +0100

    added sorting of output

--- milestone_3/call me maybe/src/__main__.py
+++ milestone_3/call me maybe/src/__main__.py
@@ -17,7 +17,15 @@ if __name__ == "__main__":
         for prompt, decoded_func in zip(parser.prompt_list, decoded):
             func_obj = json.loads(decoded_func)
             func_obj = {"prompt": prompt, **func_obj}
-            outputs.append(func_obj)
+            priority = ["prompt", "fn_name"]
+            ordered_func_obj = dict(
+                sorted(
+                    func_obj.items(),
+                    key=lambda item: priority.index(item[0]) if item[0]
+                    in priority else len(priority) + 1
+                )
+            )
+            outputs.append(ordered_func_obj)
         with open(sys.argv[6], "w") as f:
             json.dump(outputs, f, indent=2)
     except Exception as e:
